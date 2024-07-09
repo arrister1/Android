3a59e52fd67fd3a0643199de601d95b5e104e2a7
Yudhi <yudhiwinantoro@gmail.com>
2024-07-09 23:17:45 +0700

    begining peoject app

---
+++ app/src/main/java/com/synrgy7team4/bankingapps/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.synrgy7team4.bankingapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
