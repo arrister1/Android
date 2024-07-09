f944dda9fa4a06573b652b64d9093ae594a4f84d
Yudhi <yudhiwinantoro@gmail.com>
2024-07-10 00:23:05 +0700

    create: modularization for feature auth and mutasi, with depedency in it

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,21 @@
+import org.gradle.internal.impldep.org.junit.experimental.categories.Categories.CategoryFilter.include
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("androidx.navigation.safeargs.kotlin")
+    id("kotlin-parcelize")
+    kotlin("kapt")
 }
 
 android {
     namespace = "com.synrgy7team4.bankingapps"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.synrgy7team4.bankingapps"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -24,6 +29,12 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            buildConfigField("String", "BUILD_TYPE", "\"release\"")
+        }
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+            buildConfigField("String", "BUILD_FLAVOR", "\"debug\"")
         }
     }
     compileOptions {
@@ -33,15 +44,109 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+    kapt {
+        correctErrorTypes = true
+        useBuildCache = true
+    }
+}
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        val nav_version = "2.5.0"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation(project(":feature_auth"))
+    implementation(project(":feature_auth:feature_auth_data"))
+    implementation(project(":feature_auth:feature_auth_domain"))
+    implementation(project(":feature_auth:feature_auth_presentation"))
+    implementation(project(":feature_mutasi"))
+    implementation(project(":feature_mutasi:feature_mutasi_data"))
+    implementation(project(":feature_mutasi:feature_mutasi_domain"))
+    implementation(project(":feature_mutasi:feature_mutasi_presentation"))
+
+    val lifecycle_version = "2.7.0"
+    val koin_version = "3.5.6"
+
+    // ViewModel
+    //noinspection GradleDependency
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    // ViewModel utilities for Compose
+    //noinspection GradleDependency
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycle_version")
+    // LiveData
+    //noinspection GradleDependency
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    // Lifecycles only (without ViewModel or LiveData)
+    //noinspection GradleDependency
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version")
+    // Lifecycle utilities for Compose
+    //noinspection GradleDependency
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycle_version")
+
+    // Saved state module for ViewModel
+    //noinspection GradleDependency
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycle_version")
+
+    // Annotation processor
+    //noinspection GradleDependency,LifecycleAnnotationProcessorWithJava8
+    kapt("androidx.lifecycle:lifecycle-compiler:$lifecycle_version")
+
+    implementation("androidx.core:core-ktx:1.13.1")
+    //noinspection GradleDependency
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    // CircleImageView
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+
+    // DataStore
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+
+    // Facebook Shimmer Loading
+    implementation("com.facebook.shimmer:shimmer:0.5.0")
+
+    // Koin (Dependency Injection)
+    // Declare koin-bom version
+    implementation(platform("io.insert-koin:koin-bom:$koin_version"))
+
+    // Declare the koin dependencies that you need
+    implementation("io.insert-koin:koin-android")
+    implementation("io.insert-koin:koin-core-coroutines")
+    implementation("io.insert-koin:koin-androidx-workmanager")
+
+    // Play Store Services Dependencies
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+
+    // Work Manager
+    //noinspection GradleDependency
+    implementation("androidx.work:work-runtime-ktx:2.7.1")
+
+    // Chucker
+    debugImplementation("com.github.chuckerteam.chucker:library:4.0.0")
+    releaseImplementation("com.github.chuckerteam.chucker:library-no-op:4.0.0")
 }
