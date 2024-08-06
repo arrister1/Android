56405b97dd54e53ad5253fa3cafa32084d8dfadb
Moirand <andreansyah1815@gmail.com>
2024-08-06 20:48:46 +0700

    [Slicing] - Daftar Sesama Bank screen & Cek Detail Penerima

---
+++ feature_dashboard/src/main/java/com/synrgy7team4/feature_dashboard/presentation/ui/transfer/FellowAccountBankInputFragment.kt
@@ -0,0 +1,23 @@
+package com.synrgy7team4.feature_dashboard.presentation.ui.transfer
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.synrgy7team4.feature_dashboard.databinding.FragmentFellowAccountBankInputBinding
+
+class FellowAccountBankInputFragment : Fragment() {
+    private var _binding: FragmentFellowAccountBankInputBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = binding.root
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
