9ec054975c46e59faafb47987897a00ce515024d
Moirand <andreansyah1815@gmail.com>
2024-08-14 22:01:14 +0700

    [Slicing] account screen

--- feature_dashboard/src/main/java/com/synrgy7team4/feature_dashboard/presentation/ui/account/AccountFragment.kt
+++ feature_dashboard/src/main/java/com/synrgy7team4/feature_dashboard/presentation/ui/account/AccountFragment.kt
@@ -4,17 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.synrgy7team4.feature_dashboard.databinding.FragmentAccountBinding
 
 class AccountFragment : Fragment() {
-
     private var _binding: FragmentAccountBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -25,14 +20,9 @@ class AccountFragment : Fragment() {
         val accountViewModel =
             ViewModelProvider(this).get(AccountViewModel::class.java)
 
-        _binding = FragmentAccountBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        accountViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return FragmentAccountBinding.inflate(layoutInflater).also {
+            _binding = it
+        }.root
     }
 
     override fun onDestroyView() {
